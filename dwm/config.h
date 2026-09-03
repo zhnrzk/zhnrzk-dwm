950d7f9ac55cf9b9716da450e713b26b66cbb813
zhnrzk <zahinrazak@gmail.com>
2026-09-04 01:26:11 +0800

    update dwm config, add wallz wallpapers

--- dwm/config.h
+++ dwm/config.h
@@ -10,7 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "CaskaydiaMono Nerd Font:size=9" };
+static const char *fonts[]          = { "CaskaydiaMono Nerd Font:size=10" };
 static const char dmenufont[]       = "CaskaydiaMono Nerd Font:size=10";
 static const char col_gray1[]       = "#1a1b26"; /* Deep neon-blue night background */
 static const char col_gray2[]  = "#24283b"; /* Inactive border (Subtle dark purple-gray) */
